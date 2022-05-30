8a99d2063b7d2c089b52945fe533be7f077dba0d
mkalmus <mkalmus@umich.edu>
2022-05-30 18:36:13 -0400

    finish lesson 06, add animations

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,4 +30,38 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Time (use let because we change it)
+let time = Date.now()
+
+// Clock -- three.js alternative to framerate problem
+const clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
+// Animation -- is called on each frame
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    // Time so we can animate at same speed
+    // const currentTime = Date.now()
+    // high fps = smaller deltatime as we are going faster
+    // const deltaTime = currentTime - time
+    // time = currentTime
+    // Update object -- 1 rev/s
+    // mesh.rotation.z = elapsedTime * (Math.PI * 2)
+    // mesh.rotation.x = elapsedTime * (Math.PI * 2)
+
+    // Use sin to make object go up and down
+    // With cos we get a circle
+    mesh.position.y = Math.sin(elapsedTime)
+    mesh.position.x = Math.cos(elapsedTime)
+
+    // camera.lookAt(mesh.position)
+
+    // Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
